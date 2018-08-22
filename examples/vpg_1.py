83dde26821655afcc38b3fb4c2d3a61bdc010167
hl00 <40866042+hl00@users.noreply.github.com>
2018-08-22 20:28:04 +0800

    Update vpg_1.py

--- examples/vpg_1.py
+++ examples/vpg_1.py
@@ -8,32 +8,31 @@ import theano.tensor as TT
 from lasagne.updates import adam
 
 # normalize() makes sure that the actions for the environment lies
-# within the range [-1, 1] (only works for environments with continuous actions)
+# within the range [-1, 1] (only works for environments with continuous actions)确保环境的操作在[-1，1]范围内(仅适用于具有连续操作的环境)
 env = normalize(CartpoleEnv())
-# Initialize a neural network policy with a single hidden layer of 8 hidden units
+# 用8个隐藏单元的单隐层初始化一个神经网络策略
 policy = GaussianMLPPolicy(env.spec, hidden_sizes=(8,))
 
-# We will collect 100 trajectories per iteration
+# 我们将收集每一次迭代的100个轨迹。
 N = 100
-# Each trajectory will have at most 100 time steps
+# 每个轨道最多有100个时间步长。
 T = 100
-# Number of iterations
+# 迭代次数
 n_itr = 100
-# Set the discount factor for the problem
+# 设置问题的折扣因子
 discount = 0.99
-# Learning rate for the gradient update
+# 梯度更新的学习速率
 learning_rate = 0.01
 
-# Construct the computation graph
+# 构建计算图形
 
-# Create a Theano variable for storing the observations
-# We could have simply written `observations_var = TT.matrix('observations')` instead for this example. However,
-# doing it in a slightly more abstract way allows us to delegate to the environment for handling the correct data
-# type for the variable. For instance, for an environment with discrete observations, we might want to use integer
-# types if the observations are represented as one-hot vectors.
+# 创建用于存储所述观测theano变量
+#对于这个例子，我们可以简单地编写 `observations_var = TT.matrix('observations')`。
+# 但是，以一种稍微抽象的方式执行它，可以让我们委托环境来处理变量的正确数据类型。
+# 例如，对于具有离散观测的环境，我们可能需要使用整数。
 observations_var = env.observation_space.new_tensor_variable(
     'observations',
-    # It should have 1 extra dimension since we want to represent a list of observations
+    # 它应该有一个额外的维度，因为我们想要表示一个观察列表。
     extra_dims=1
 )
 actions_var = env.action_space.new_tensor_variable(
@@ -42,20 +41,20 @@ actions_var = env.action_space.new_tensor_variable(
 )
 returns_var = TT.vector('returns')
 
-# policy.dist_info_sym returns a dictionary, whose values are symbolic expressions for quantities related to the
-# distribution of the actions. For a Gaussian policy, it contains the mean and the logarithm of the standard deviation.
+# policy.dist_info_sym 返回一个字典，其值是与操作分布相关的数量的符号表达式。
+# 对于高斯策略，它包含标准差的均值和对数。
 dist_info_vars = policy.dist_info_sym(observations_var)
 
-# policy.distribution returns a distribution object under rllab.distributions. It contains many utilities for computing
-# distribution-related quantities, given the computed dist_info_vars. Below we use dist.log_likelihood_sym to compute
-# the symbolic log-likelihood. For this example, the corresponding distribution is an instance of the class
-# rllab.distributions.DiagonalGaussian
+# policy.distribution returns a distribution object under rllab.distributions. 
+# 它包含许多用于计算与分发相关的数量的实用程序，给定计算出的dist_info_vars. 
+# 下面，我们使用dist.log_likelihood_sym 来计算symbolic log-likelihood. 
+# 在这个例子中，相应的分布是类rllab.distributions.DiagonalGaussian的一个实例。
 dist = policy.distribution
 
-# Note that we negate the objective, since most optimizers assume a minimization problem
+# 请注意，我们否定目标，因为大多数优化器假设最小化问题。
 surr = - TT.mean(dist.log_likelihood_sym(actions_var, dist_info_vars) * returns_var)
 
-# Get the list of trainable parameters.
+# 获取可训练参数的列表
 params = policy.get_params(trainable=True)
 grads = theano.grad(surr, params)
 
@@ -78,31 +77,27 @@ for _ in range(n_itr):
         observation = env.reset()
 
         for _ in range(T):
-            # policy.get_action() returns a pair of values. The second one returns a dictionary, whose values contains
-            # sufficient statistics for the action distribution. It should at least contain entries that would be
-            # returned by calling policy.dist_info(), which is the non-symbolic analog of policy.dist_info_sym().
-            # Storing these statistics is useful, e.g., when forming importance sampling ratios. In our case it is
-            # not needed.
+            # policy.get_action() 返回一对值。 第二个返回字典，其值包含足够的动作分布统计信息。
+            # 它至少应该包含通过调用policy.dist_info()来返回的条目，后者是policy.dist_info_sym()的非符号模拟。
+            # 存储这些统计数据是有用的，例如，在形成重要抽样比率时。在我们的例子中，它是不需要的。
             action, _ = policy.get_action(observation)
-            # Recall that the last entry of the tuple stores diagnostic information about the environment. In our
-            # case it is not needed.
+            # 回想一下，元组的最后一个条目存储有关环境的诊断信息。在我们的情况下，这是不必要的。
             next_observation, reward, terminal, _ = env.step(action)
             observations.append(observation)
             actions.append(action)
             rewards.append(reward)
             observation = next_observation
             if terminal:
-                # Finish rollout if terminal state reached
+                # Finish rollout if terminal state reached如果到达终端状态，则完成显示
                 break
 
-        # We need to compute the empirical return for each time step along the
-        # trajectory
+        # 我们需要计算沿着轨道的每一个时间步骤的经验回报。
         returns = []
         return_so_far = 0
         for t in range(len(rewards) - 1, -1, -1):
             return_so_far = rewards[t] + discount * return_so_far
             returns.append(return_so_far)
-        # The returns are stored backwards in time, so we need to revert it
+        # 返回是向后存储的，所以我们需要恢复它。
         returns = returns[::-1]
 
         paths.append(dict(
